dc39b74af33dac4b70043aad40fdda37872ef01c
XX-salamandra-XX <123819241+XX-salamandra-XX@users.noreply.github.com>
2023-02-02 12:53:09 +0000

    выравнивание address

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,7 +3,7 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-4 mt-2 col roundTrip px-4 py-4'>
+        <div className='d-inline-flex flex-column gap-4 mt-1 col roundTrip px-4 py-4'>
             <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
