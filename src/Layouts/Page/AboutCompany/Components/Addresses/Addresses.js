9c1f9bb1b2226dba48adbc586d41e2d39c815fc6
XX-salamandra-XX <123819241+XX-salamandra-XX@users.noreply.github.com>
2023-02-03 14:53:09 +0000

    adress отступы финал

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,14 +3,16 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-5 mt-1 col roundTrip px-4 py-4 justify-content-between'>
+        <div className='d-inline-flex flex-column me-5 col roundTrip px-4 py-4 justify-content-between'>
             <div>
               <div className='title'>Адреса заведений</div>
+              <div className='d-flex flex-column gap-5 py-5'>
                     <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                     <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                </div>
-                    <SecondaryButton>Добавить филиал</SecondaryButton>
+                    </div>
             </div>
+                    <SecondaryButton>Добавить филиал</SecondaryButton>
+        </div>
     );
 }
 
