69f29f605db48db0825f50618ba999a79f2204fa
XX-salamandra-XX <123819241+XX-salamandra-XX@users.noreply.github.com>
2023-02-03 10:56:51 +0000

    address откатила

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,9 +3,9 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-4 mt-1 col roundTrip px-4 py-4'>
-            <div className='d-inline-flex flex-column justify-content-between gap-4'>
+        <div className='d-inline-flex flex-column gap-5 mt-1 col roundTrip px-4 py-4'>
             <div className='title'>Адреса заведений</div>
+            <div className='d-inline-flex flex-column gap-5'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
