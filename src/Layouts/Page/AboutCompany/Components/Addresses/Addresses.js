318129658e404bc248c9a855b4869885e8333a05
XX-salamandra-XX <123819241+XX-salamandra-XX@users.noreply.github.com>
2023-02-03 14:18:59 +0000

    Adress div

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,14 +3,14 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-5 mt-1 col roundTrip px-4 py-4'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-5'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
+        <div className='d-inline-flex flex-column gap-5 mt-1 col roundTrip px-4 py-4 justify-content-between'>
+            <div>
+              <div className='title'>Адреса заведений</div>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
+                    <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
-        </div>
     );
 }
 
